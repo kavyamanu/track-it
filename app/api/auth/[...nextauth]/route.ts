412eca9b75743d9739500a2c97e0c9638347a56e
kavyamanu <kavyamanupati11@gmail.com>
2023-11-05 14:25:16 +0530

    feat: initial setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+});
+
+export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,167 @@
-import Image from 'next/image'
+"use client";
+
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
+  format,
+  getDay,
+  isFuture,
+  isToday,
+  parse,
+  startOfToday,
+  startOfWeek,
+} from "date-fns";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
+import { SessionProvider } from "next-auth/react";
+import Link from "next/link";
+import Image from "next/image";
+
+export const capitalize = (query: string): string => {
+  return query.charAt(0).toUpperCase() + query.substring(1);
+};
 
 export default function Home() {
+  const today = startOfToday();
+  const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  const colStartClasses = [
+    "",
+    "col-start-2",
+    "col-start-3",
+    "col-start-4",
+    "col-start-5",
+    "col-start-6",
+    "col-start-7",
+  ];
+  const [date, setDate] = useState([]);
+  const saveDates = (day) => {
+    if (!date.includes(day)) setDate((date) => [day, ...date]);
+    else setDate((date) => date.filter((e) => e !== day));
+  };
+  console.log(date);
+  const [currMonth, setCurrMonth] = useState(() => format(today, "MMM-yyyy"));
+  let firstDayOfMonth = parse(currMonth, "MMM-yyyy", new Date());
+
+  const daysInMonth = eachDayOfInterval({
+    start: startOfWeek(firstDayOfMonth),
+    end: endOfWeek(endOfMonth(firstDayOfMonth)),
+  });
+
+  const getPrevMonth = (event: React.MouseEvent<SVGSVGElement>) => {
+    event.preventDefault();
+    const firstDayOfPrevMonth = add(firstDayOfMonth, { months: -1 });
+    setCurrMonth(format(firstDayOfPrevMonth, "MMM-yyyy"));
+  };
+
+  const getNextMonth = (event: React.MouseEvent<SVGSVGElement>) => {
+    event.preventDefault();
+    const firstDayOfNextMonth = add(firstDayOfMonth, { months: 1 });
+    setCurrMonth(format(firstDayOfNextMonth, "MMM-yyyy"));
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-around p-12">
+      <div className="flex place-items-center justify-around items-center w-screen">
+        <Link type="button" href="/">
+          <Image
+            src="/trackit.png"
+            width={80}
+            height={80}
+            alt="logo"
+            className="border border-red-300 rounded-3xl"
+          />
+        </Link>
+        <div className="flex flex-col items-center mt-6">
+          <p className="text-5xl text-red-500 font-bold p-4">
+            My Period Tracker
+          </p>
+          <p className="text-gray-500">Be a bit stronger everyday</p>
+        </div>
+        <Link
+          className="rounded-full bg-red-600 px-8 py-2.5 text-m font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          href="/api/auth/signin"
+        >
+          SIGN IN
+        </Link>
+      </div>
+
+      <div className="p-8 w-screen h-4/5 flex items-center justify-center">
+        <div className="w-[900px] h-[600px]">
+          <div className="flex items-center justify-between">
+            <p className="font-semibold text-xl">
+              {format(firstDayOfMonth, "MMMM yyyy")}
+            </p>
+            <div className="flex items-center justify-evenly gap-6 sm:gap-12">
+              <ChevronLeftIcon
+                className="w-6 h-6 cursor-pointer"
+                onClick={getPrevMonth}
+              />
+              <ChevronRightIcon
+                className="w-6 h-6 cursor-pointer"
+                onClick={getNextMonth}
+              />
+            </div>
+          </div>
+          <hr className="my-6" />
+          <div className="grid grid-cols-7 gap-6 sm:gap-12 place-items-center">
+            {days.map((day, idx) => {
+              return (
+                <div key={idx} className="font-semibold">
+                  {capitalize(day)}
+                </div>
+              );
+            })}
+          </div>
+          <div className="grid grid-cols-7 gap-6 sm:gap-12 mt-8 place-items-center">
+            {daysInMonth.map((day, idx) => {
+              return (
+                <div key={idx} className={colStartClasses[getDay(day)]}>
+                  <p
+                    className={`cursor-pointer flex items-center justify-center font-semibold h-8 w-8 rounded-full ${
+                      isFuture(day) ? "text-gray-400" : "text-gray-900"
+                    } ${!isToday(day) && "hover:bg-red-200"} ${
+                      isToday(day) && "border border-red-500"
+                    } ${
+                      date.includes(format(day, "d MMM yyyy")) &&
+                      " bg-red-500 text-white"
+                    }`}
+                    onClick={() => saveDates(format(day, "d MMM yyyy"))}
+                  >
+                    {format(day, "d")}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="gap-5 flex flex-col w-screen items-center h-20 overflow-scroll">
+        <p className="text-2xl text-pink-600 font-medium">Period History</p>
+        <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
+          {date[0] &&
+            date.map((eachDate, index) => (
+              <li className="flex items-center space-x-3 " key={index}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-red-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                  />
+                </svg>
+                <span className="font-bold">{eachDate}</span>
+              </li>
+            ))}
+        </ul>
       </div>
     </main>
-  )
+  );
 }
